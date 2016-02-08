4f6834cf10af81b1b14debb3ada85c58c5404018
Toussaint BOOS <tboos@e1r10p7.42.fr>
2016-02-08 16:38:52 +0100

    fin

--- srcs/ft_reorder.c
+++ srcs/ft_reorder.c
@@ -6,60 +6,41 @@
 /*   By: tboos <toussaint.boos@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 18:15:02 by tboos             #+#    #+#             */
-/*   Updated: 2016/02/05 06:14:20 by tboos            ###   ########.fr       */
+/*   Updated: 2016/02/08 16:38:38 by tboos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-t_tetriminos	*ft_reorder(t_tetriminos *rabbit)
+t_tetriminos	*ft_reorder(t_tetriminos **arrow, int stage, int len)
 {
-	t_tetriminos	*stick;
-	t_tetriminos	*turtle;
+	int				i;
+	int				m;
 
-	if (rabbit->prev)
-		ft_remouve_tetris(rabbit->prev);
-	stick = rabbit;
-	while (stick && stick->next)
+	if ((stage - 1) >= 0)
+		ft_remouve_tetris(arrow[stage - 1]);
+	while (stage < len)
 	{
-		rabbit = stick->next;
-		turtle = stick;
-		while (rabbit)
+		i = stage + 1;
+		m = stage;
+		while (i <= len)
 		{
-			if (rabbit->id < turtle->id)
-				turtle = rabbit;
-			rabbit = rabbit->next;
+			if (arrow[i]->id < arrow[m]->id)
+				m = i;
+			i++;
 		}
-		if (turtle != stick)
-		{
-			ft_tetriswap(turtle, stick);
-			stick = turtle->next;
-		}
-		else
-			stick = stick->next;
+		if (m != stage)
+			ft_tetriswap(arrow, stage, m);
+		stage++;
 	}
 	return (NULL);
 }
 
-void		ft_tetriswap(t_tetriminos *r1, t_tetriminos *r2)
+void		ft_tetriswap(t_tetriminos **arrow, int stage, int i)
 {
 	t_tetriminos	*s;
 
-	if (r1 && r2)
-	{
-		if (r1->prev)
-			r1->prev->next = r2;
-		if (r2->prev)
-			r2->prev->next = r1;
-		s = r1->prev;
-		r1->prev = r2->prev;
-		r2->prev = s;
-		if (r2->next)
-			r2->next->prev = r1;
-		if (r1->next)
-			r1->next->prev = r2;
-		s = r1->next;
-		r1->next = r2->next;
-		r2->next = s;
-	}
+	s = arrow[stage];
+	arrow[stage] = arrow[i];
+	arrow[i] = s;
 }
--- srcs/ft_tetriorder.c
+++ srcs/ft_tetriorder.c
@@ -6,36 +6,27 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 18:14:15 by tboos             #+#    #+#             */
-/*   Updated: 2016/02/05 10:54:40 by fjanoty          ###   ########.fr       */
+/*   Updated: 2016/02/08 16:38:33 by tboos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "header.h"
 
-static t_tetriminos 	*ft_followrightrabbit(t_tetriminos *rabbit, short *i, int *readymade)
+static void				ft_followrightrabbit(t_tetriminos **arrow, int *i, int len, int *readymade)
 {
-	short			tmp;
-
-	tmp = *i;
-	while (tmp && rabbit)
-	{
-		rabbit = rabbit->next;
-		tmp--;
-	}
-	while (rabbit && (*readymade & rabbit->type))
-	{
-		rabbit = rabbit->next;
+	while (i <= len && (*readymade & (arrow[*i])->type))
 		(*i)++;
+	if (i <= len)
+	{
+		*readymade += (arrow[*i])->type;
+		return (1);
 	}
-	if (rabbit)
-		*readymade += rabbit->type;
-	return (rabbit);
+	return (0);
 }
 
-t_tetriminos			**ft_tetriorder(t_tetriminos **turtle, int len, int stage)
+t_tetriminos			**ft_tetriorder(t_tetriminos **arrow, int len, int stage)
 {
-	short			i;
-	short			rabbit;
+	int				i;
 	int				readymade;
 	t_tetriminos	**test;
 
@@ -43,21 +34,19 @@ t_tetriminos			**ft_tetriorder(t_tetriminos **turtle, int len, int stage)
 		return (arrow);
 	else if (stage == len)
 		return (ft_reorder(arrow, stage));
-	i = 0;
+	i = stage;
 	readymade = 0;
 	while (++i <= len - stage || rabbit)
 	{
-		if (ft_push_tetriminos(turtle))
-			if ((test = ft_tetriorder(turtle->next, len, stage + 1)))
+		if (ft_push_tetriminos(arrow[stage]))
+			if ((test = ft_tetriorder(arrow, len, stage + 1)))
 				return (test);
 		else
 			return (ft_reorder(arrow, stage));
-		rabbit = ft_followrightrabbit(arrow, stage, len, &readymade);
-		ft_tetriswap(arrow, stage, rabbit);
-		if (rabbit)
-			turtle = rabbit;
+		if ((ft_followrightrabbit(arrow, &i, len, &readymade)))
+			ft_tetriswap(arrow, stage, i);
 	}
-	return (ft_reorder(arrow, stage));
+	return (ft_reorder(arrow, stage, len));
 }
 
 t_tetriminos			**ft_squ_lunch(t_tetriminos **arrow, int len)
